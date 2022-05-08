ee99ec66eaf43896945f57b0b21cb08af0ce875b
Ayush Iyankan <75990868+AyushIyankan@users.noreply.github.com>
2022-05-08 21:59:08 +0530

    fix header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import MenuIcon from "@material-ui/icons/Menu";
 
 function Header() {
   return (
@@ -11,7 +12,21 @@ function Header() {
         <p>
           <a href="#">Model S</a>
         </p>
+        <p>
+          <a href="#">Model 3</a>
+        </p>
+        <p>
+          <a href="#">Model X</a>
+        </p>
+        <p>
+          <a href="#">Model Y</a>
+        </p>
       </Menu>
+      <RightMenu>
+        <a href="#">Shop</a>
+        <a href="#">Tesla Account</a>
+      </RightMenu>
+      <CustomMenu />
     </Container>
   );
 }
@@ -23,6 +38,44 @@ const Container = styled.div`
   position: fixed;
   display: flex;
   align-items: center;
+  justify-content: space-between
   padding: 0 20px;
+  top: 0;
+  left: 0;
+  right: 0;
 `;
-const Menu = styled.div``;
+const Menu = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #393c41;
+  flex: 1;
+
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    padding: 0 10px;
+    flex-wrap: no-wrap;
+    color: #393c41;
+  }
+
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const RightMenu = styled.div`
+  display: flex;
+  align-items: center;
+
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    margin-right: 10px;
+    color: #393c41;
+  }
+`;
+
+const CustomMenu = styled(MenuIcon)`
+  cursor: pointer;
+`;
